cf460c43d3aaf4d642a17d6f76aed20437a8320a
danilo <danilodramos@yahoo.com.br>
2024-01-29 11:22:22 -0300

    projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./components/pages/Home";
-import Company from "./components/pages/Company";
-import Contact from "./components/pages/Contact";
-import NewProject from "./components/pages/NewProject";
-import Container from "./components/layout/Container";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
+import Container from './components/layout/Container'
+import Home from './components/pages/Home'
+import Projects from './components/pages/Project'
+import NewProject from './components/pages/NewProject'
+import Company from './components/pages/Company'
+import Contact from './components/pages/Contact'
+import Project from './components/pages/Project'
 
 function App() {
   return (
-
     <Router>
-
-      <div>
-      <ul>
-        <Link to="/" >Home</Link>
-        <Link to="/contact" >Contato</Link>
-        <Link to="company" >Empresa</Link>
-        <Link to="newproject" >Novo Projeto</Link>
-      </ul>
-      </div>
-
-      <Container>
+      <Navbar />
       <Routes>
-        <Route path="/" element= {< Home />}/>
-        <Route path="/company" element= {< Company />}/>
-        <Route path="/contact" element= {< Contact />}/>
-        <Route path="newproject" element= {< NewProject />}/>
+        <Container customClass="min-height">
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/company">
+            <Company />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/newproject">
+            <NewProject />
+          </Route>
+          <Route path="/project/:id">
+            <Project />
+          </Route>
+        </Container>
       </Routes>
-      </Container>
-
-      <footer>Rodapé</footer>
-
+      <Footer />
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
